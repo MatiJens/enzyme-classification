4c529f533ddf98512f6b01335f56f0b25db14cf4
Mateusz <mateuszjens14@gmail.com>
2025-06-03 18:23:36 +0200

    Final

--- app_utils/enzyme_predict.py
+++ app_utils/enzyme_predict.py
@@ -5,30 +5,41 @@ from esm.sdk.api import ESMProtein, LogitsConfig
 from utils.esmc_embedding import esmc_embedding
 
 def enzyme_predict(seq, client, model):
+    # Valid aminoacids
+    valid_aac = 'ACDEFGHIKLMNPQRSTVWY'
+    # Check if sequence is string
+    if isinstance(seq, str):
+        # Check if sequence have only valid aminoacids
+        if all(aac in valid_aac for aac in seq):
+            # Encode sequence
+            protein = ESMProtein(sequence=seq)
+            protein_tensor = client.encode(protein)
+            # Create logit of it
+            logits_output = client.logits(
+                protein_tensor, LogitsConfig(return_embeddings=True)
+            )
+            # Extract embedding
+            embedding = logits_output.embeddings
+            # Extract global embedding vector
+            global_embb = embedding[0, 0, :]
+            with torch.no_grad():
+                # Use model to predict enzyme/not enzyme
+                predict_MLP = model(global_embb)
+            # Calculate the prediction
+            predict_prob = torch.sigmoid(predict_MLP)
+            predict_class = ((predict_prob > 0.5).float().squeeze()).item()
 
-    # Encode sequence
-    protein = ESMProtein(sequence=seq)
-    protein_tensor = client.encode(protein)
-    # Create logit of it
-    logits_output = client.logits(
-        protein_tensor, LogitsConfig(return_embeddings=True)
-    )
-    # Extract embedding
-    embedding = logits_output.embeddings
-    # Extract global embedding vector
-    global_embb = embedding[0, 0, :]
-    with torch.no_grad():
-        # Use model to predict enzyme/not enzyme
-        predict_MLP = model(global_embb)
-    # Calculate the prediction
-    predict_prob = torch.sigmoid(predict_MLP)
-    predict_class = ((predict_prob > 0.5).float().squeeze()).item()
+            if predict_class == 1:
+                probability = predict_prob.item()
+                result = 'enzyme'
+            else:
+                result = 'not enzyme'
+                probability = 1 - predict_prob.item()
 
-    if predict_class == 1:
-        probability = predict_prob.item()
-        result = 'enzyme'
+            return result, probability
+        else:
+            result = 'Sequence have not valid aminoacids'
+            return result
     else:
-        result = 'not enzyme'
-        probability = 1 - predict_prob.item()
-
-    return result, probability
+        result = 'Sequence is not string'
+        return result
